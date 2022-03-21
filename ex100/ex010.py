cc5d51fafeb4de21f51bd3f0239669356126678b
MJ <pdsmajian@126.com>
2022-03-21 15:44:42 +0800

    10

---
+++ ex100/ex010.py
@@ -0,0 +1,9 @@
+# 题目：暂停一秒输出，并格式化当前时间。
+import time
+
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+# 暂停一秒
+time.sleep(3)
+
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
